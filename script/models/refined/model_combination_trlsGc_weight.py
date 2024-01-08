cb8c8302d12418bc00efb2038122b2c87c61f30c
AIMed <136410529+AIMedDrug@users.noreply.github.com>
2024-01-08 16:17:52 +0800

    model_combination_trlsGc_weight.py

--- script/models/refined/model_combination_trlsGc_weight.py
+++ script/models/refined/model_combination_trlsGc_weight.py
@@ -65,15 +65,20 @@ class comModel(nn.Module):
         # 图处理层
         self.gcn_conv1 = GCNConv(self.num_features_x, self.num_features_x)
         self.gcn_conv2 = GCNConv(self.num_features_x, self.num_features_x * 2)
- 
-       
 
+        self.num_attention_heads = 2
+        self.ep_attention_layers = nn.ModuleList([nn.Sequential(
+            nn.Linear(2 * self.hidden_dim, self.hidden_dim),
+            nn.Tanh(),
+            nn.Linear(self.hidden_dim, self.output_dim)
+        ) for _ in range(self.num_attention_heads)])
+        self.ep_fc_layers = nn.ModuleList([nn.Linear(self.hidden_dim * 2, self.output_dim) for _ in range(self.num_attention_heads)])
+        
         self.dropout = nn.Dropout()
-
 
         self.smi_fc1 = nn.Linear(self.num_features_smi, self.n_output)
         self.smi_fc2 = nn.Linear(self.hidden_dim, self.n_output)
-        self.ep_fc1 = nn.Linear(2*self.hidden_dim, self.n_output)
+        self.ep_fc1 = nn.Linear(self.output_dim, self.n_output)
         self.smi_ep_fc1 = nn.Linear(self.output_dim, self.num_features_x*2)
         self.gc_fc = nn.Linear(2*self.num_features_x, self.n_output)
     
@@ -84,24 +89,21 @@ class comModel(nn.Module):
         smi_ln = self.smi_fc1(smi_encoded)
         smi_ln = smi_ln.view(smi_ln.shape[0], -1)
         smi_out = self.smi_fc2(smi_ln).squeeze()
-    
         
         ep_gru, _ = self.ep_gru(ecfp)
-        ep_out = self.dropout(ep_gru)
+        ep_attended_out = 0
+        for i in range(self.num_attention_heads):
+            ep_attention_weights = torch.softmax(self.ep_attention_layers[i](ep_gru), dim=1)
+            ep_linear = self.ep_fc_layers[i](ep_gru)
+            ep_attended_out += ep_attention_weights * ep_linear
+        ep_attended_out /= self.num_attention_heads
         ep_out = self.ep_fc1(ep_out).squeeze()
-
-
-
         x = self.gcn_conv1(x, edge_index)
         x = F.relu(x)
         x = self.gcn_conv2(x, edge_index)
         x = F.relu(x)
         gc = gap(x, batch)
         gc_out = self.gc_fc(gc).squeeze()
-        
-  
-
-        
         return smi_out, ep_out, gc_out
 
 
